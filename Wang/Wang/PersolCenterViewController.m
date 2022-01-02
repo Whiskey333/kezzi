4c0e1d002b4e2b50f0dcea184a9e83d0d4d83ab0
Whiskey333 <www_2279@qq.com>
2022-01-02 14:58:14 +0800

    添加标题咯

--- Wang/Wang/PersolCenterViewController.m
+++ Wang/Wang/PersolCenterViewController.m
@@ -22,6 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.title = @"四季花"；
     
     self.persolCenterListTableView.backgroundColor = [UIColor blueColor];
     UINib *nib = [UINib nibWithNibName:@"PersolCenterListTableViewCell" bundle:nil];
